7cd3f740e84593c9edbfb718ee6d8f4f0eb07f69
npellan <npellan@gmail.com>
2021-02-08 16:46:53 +0100

    import React Model

---
+++ src/components/Chatroom/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Chatroom = () => (
+  <div className="chatroom">
+    <h1>Composant : Chatroom</h1>
+  </div>
+);
+
+// == Export
+export default Chatroom;
