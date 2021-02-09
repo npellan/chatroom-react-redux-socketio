1a4a2f7d7ebe826f5ade49955d0016422f776101
npellan <npellan@gmail.com>
2021-02-09 14:27:24 +0100

    scroll to bottom when new message

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // == Import
@@ -8,20 +8,33 @@ import './styles.scss';
 import Message from './Message';
 
 // == Composant
-const Messages = ({ messages }) => (
-  <div className="messages">
-    {messages.map(
-      (message) => (
-        <Message
-          key={message.id}
-          sender={message.sender}
-          content={message.content}
-          isFriend={message.isFriend}
-        />
-      ),
-    )}
-  </div>
-);
+const Messages = ({ messages }) => {
+  const messagesContainer = useRef(null);
+
+  const scrollToMyRef = () => {
+    const scroll = messagesContainer.current.scrollHeight - messagesContainer.current.clientHeight;
+    messagesContainer.current.scrollTo(0, scroll);
+  };
+
+  useEffect(() => {
+    scrollToMyRef();
+  }, [messages]);
+
+  return (
+    <div ref={messagesContainer} className="messages">
+      {messages.map(
+        (message) => (
+          <Message
+            key={message.id}
+            sender={message.sender}
+            content={message.content}
+            isFriend={message.isFriend}
+          />
+        ),
+      )}
+    </div>
+  );
+};
 
 Messages.propTypes = {
   messages: PropTypes.arrayOf(
